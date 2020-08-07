003416c1124d27086bb393e158c414492a164318
pupul.mwm <pupul.mwm@gmail.com>
2020-08-07 15:33:48 +0800

    revised push stack on file master

--- web-statis-laravel/resources/views/adminlte/master.blade.php
+++ web-statis-laravel/resources/views/adminlte/master.blade.php
@@ -56,12 +56,7 @@
     <script src="../../dist/js/demo.js"></script>
     <script src="../../plugins/datatables/jquery.dataTables.js"></script>
     <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-    <script>
-        $(function() {
-            $("#example1").DataTable();
-        });
 
-    </script>
     @stack('scripts')
 </body>
 
